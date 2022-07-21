5a39404b8b920dc31d48546b22150dbf7ec6f1ca
Daniel <danielazimi101@gmail.com>
2022-07-21 13:20:28 -0400

    Initial commit.

---
+++ SOSCSRPG/Engine/CharacterModels/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.CharacterModels
+{
+    internal class Player
+    {
+        string PlayerName { get; set; }
+        string PlayerClass { get; set; }
+        int HitPoints { get; set; }
+        int ExpPoints { get; set; }
+        int Level { get; set; }
+        int Gems { get; set; }
+    }
+}
